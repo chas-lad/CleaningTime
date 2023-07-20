193060f31092dd57564495717883a7370a9b2a92
Chas Ladhar <chasladhar@Chass-MacBook-Pro.local>
2023-07-20 22:16:38 +0100

    Readme

---
+++ CleaningTime/CleaningTime/CleaningTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleaningTimeApp.swift
+//  CleaningTime
+//
+//  Created by Chas Ladhar on 20/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CleaningTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
